7b051832cb356001d3fb6ada469e023bdae5fbf4
Nicolas <nick.nogueira02@gmail.com>
2025-03-17 21:13:21 -0300

    calculadora funcionando

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  num1: number = 0;
+  num2: number = 0;
+  sum: number = 0;
 
+  onClick(op:string){
+    switch(op){
+    case 'Somar': this.sum = this.num1 + this.num2;break
+    case 'Subtrair': this.sum = this.num1 - this.num2; break
+    case 'Dividir': this.sum = this.num1 / this.num2; break
+    case 'Multiplicar': this.sum = this.num1 * this.num2; break
+  }
+}
+  
 }
